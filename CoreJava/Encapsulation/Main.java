f4f7bf750d5838d86212db17c9b457f93a5f4ccf
ShoebFarooqui28 <farooquishoeb2@gmail.com>
2024-07-11 05:06:49 +0530

    Changes

---
+++ CoreJava/Encapsulation/Main.java
@@ -0,0 +1,19 @@
+package CoreJava.Encapsulation;
+
+public class Main {
+    public static void main(String[] args) {
+        Car car = new Car("Toyota", "Civic",2023);
+
+        //System.out.println(car.make);   //error because the variable is private
+
+        System.out.println(car.getMake());
+        System.out.println(car.getModel());
+        System.out.println(car.getYear());
+
+
+        car.setMake("hello");
+        System.out.println(car.getMake());        
+    }
+
+
+}
