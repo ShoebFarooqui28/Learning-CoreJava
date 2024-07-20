483fefc3546fbb9948ef09166203887f92bf1ffe
ShoebFarooqui28 <farooquishoeb2@gmail.com>
2024-07-20 06:30:23 +0530

    Changes

--- BasicTicTacToe/Main.java
+++ BasicTicTacToe/Main.java
@@ -1,4 +1,4 @@
-package TicTacToe;
+package BasicTicTacToe;
 
 import java.util.Scanner;
 
--- TicTacToeGUI/Main.java
+++ TicTacToeGUI/Main.java
@@ -2,6 +2,7 @@ package TicTacToeGUI;
 
 public class Main {
     public static void main(String[] args) {
+        @SuppressWarnings("unused")
         TicTacToe game = new TicTacToe();
     }
 }
