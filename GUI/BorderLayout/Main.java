ca64dbf005395501744b4a769e01cbc5b39716e9
ShoebFarooqui28 <farooquishoeb2@gmail.com>
2024-08-01 19:47:40 +0530

    Commit

--- GUI/BorderLayout/Main.java
+++ GUI/BorderLayout/Main.java
@@ -1,7 +1,13 @@
 package GUI.BorderLayout;
 
+import javax.swing.JFrame;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println();
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500, 500);
+        frame.setVisible(true);
+        
     }
 }
