d34d9f2a4fbe6d6305d201103365a1437e8fa443
ShoebFarooqui28 <farooquishoeb2@gmail.com>
2024-08-01 23:14:20 +0530

    New Topics

---
+++ GUI/NewWindow/Main.java
@@ -0,0 +1,8 @@
+package GUI.NewWindow;
+
+public class Main {
+    public static void main(String[] args) {
+        @SuppressWarnings("unused")
+        LaunchPage launchPage = new LaunchPage();
+    }
+}
